9a2338daec8d72abec71f64c7bb99889911e8fac
ERICK-NJERU <enjeru6@gmail.com>
2025-06-19 08:46:17 +0300

    file

---
+++ MADLIBS GAME.py
@@ -0,0 +1,12 @@
+
+verb1 = input("Enter a verb: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+place1 = input("Enter a place: ")
+verb2 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")  
+
+print(f"today i was {verb1} a football{noun1} with my {noun2} and {noun3} in the {place1} \
+and i was {verb2} a {adjective1}")
+
